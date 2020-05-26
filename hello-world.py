c299a0e45121e2ff9797820156c5b6e43dbc6248
Ninda <aiswra@users.noreply.github.com>
2020-05-27 00:37:04 +0700

    load simple flask web

---
+++ hello-world.py
@@ -0,0 +1,11 @@
+from flask import Flask, redirect, url_for, render_template
+
+app = Flask(__name__)
+
+@app.route("/")
+def home():
+	return render_template("index.html", content="Rasyid")
+if __name__ == "__main__":
+	app.run()
+	
+
