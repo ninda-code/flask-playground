d23b0774516b4316574521dd6b73d4d8292b0420
Ninda <aiswra@users.noreply.github.com>
2021-05-16 16:00:31 +0700

    update

--- hello-world.py
+++ hello-world.py
@@ -5,7 +5,18 @@ app = Flask(__name__)
 @app.route("/")
 def home():
 	return render_template("index.html", content="Rasyid")
+
+@app.route("/login", methods=["POST","GET"])
+def login():
+	return render_template()
+	
+@app.route("/<usr>")
+def user(usr):
+	return f"<h1>{usr}</h1>"
+
+
 if __name__ == "__main__":
 	app.run()
+	
 	
 
